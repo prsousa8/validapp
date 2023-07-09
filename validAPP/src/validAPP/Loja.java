5fa59f1a56b0e42166e5329d424dd31c249c0ed2
Isaac Daniel <isaacbusiness2019@hotmail.com>
2023-07-09 12:00:05 -0300

    Aplicando o padrão de construção Singleton

--- validAPP/src/validAPP/Loja.java
+++ validAPP/src/validAPP/Loja.java
@@ -1,10 +1,35 @@
 package validAPP;
 
-public class Loja {
-	public String cnpj;
-	public Estoque estoque;
+public final class Loja {
+	private String cnpj;
+	private Estoque estoque;
+	private static Loja instanciaLoja;
 	
-	public Loja(String cnpj, Estoque estoque) {
+	private Loja(String cnpj, Estoque estoque) {
+		this.cnpj = cnpj;
 		this.estoque = estoque;
+	}
+	
+	public static Loja getLoja() {
+		if (instanciaLoja != null)
+			return instanciaLoja;
+		
+		Estoque E = new Estoque();
+		
+		E.criar_e_adicionar_lote_ao_estoque("limao", 15.50, "frutas", 7, "10/10/2022", "C0D09", "setor 10");
+		E.criar_e_adicionar_lote_ao_estoque("uva", 15.50, "frutas", 5, "10/10/2024", "C0D010", "setor 10");
+		E.criar_e_adicionar_lote_ao_estoque("maca", 5.00, "frutas", 5, "10/07/2023", "C0D011", "setor 10");
+
+		instanciaLoja = new Loja("00220000", E);
+
+		return instanciaLoja;
+	}
+	
+	public Estoque getEstoque() {
+		return this.estoque;
+	}
+	
+	public String getCnpj() {
+		return this.cnpj;
 	}
 }
--- validAPP/src/validAPP/main.java
+++ validAPP/src/validAPP/main.java
@@ -1,28 +1,15 @@
 package validAPP;
 
-import java.util.ArrayList;
-
-import javax.swing.JFrame;
-
 public class main {
-
-	public static void main(String[] args) {
-		Estoque E = new Estoque();
-			
-		E.criar_e_adicionar_lote_ao_estoque("limao", 15.50, "frutas", 7, "10/10/2022", "C0D09", "setor 10");
-		E.criar_e_adicionar_lote_ao_estoque("uva", 15.50, "frutas", 5, "10/10/2024", "C0D010", "setor 10");
-		E.criar_e_adicionar_lote_ao_estoque("maca", 5.00, "frutas", 5, "10/07/2023", "C0D011", "setor 10");
+	public static void main(String[] args) {		
+		Loja loja = Loja.getLoja();
 		
-		Loja L = new Loja("000222333", E); 
+		Verificacao verificacao = new Verificacao();
+		loja.getEstoque().imprimir();
+		verificacao.verificar_e_printar_lotes(loja.getEstoque());
 		
-		Verificacao V = new Verificacao();
-		E.imprimir();
-		V.verificar_e_printar_lotes(E);
-		
-		
-		//interface gráfica
-		Notificacao N = new Notificacao();
-		N.gerarInterfaceGrafica(E.lotes);
-		
+		//Interface Gráfica
+		Notificacao notificacao = new Notificacao();
+		notificacao.gerarInterfaceGrafica(loja.getEstoque().lotes);
 	}
 }
